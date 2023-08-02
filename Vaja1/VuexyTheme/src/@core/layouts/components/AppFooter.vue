5d2a037ce508599d3597b983dfa5b57c3e1cd18b
Rene Potocnik <rene.potocnik1@gmail.com>
2023-08-02 15:05:09 +0200

    🦶er

--- Vaja1/VuexyTheme/src/@core/layouts/components/AppFooter.vue
+++ Vaja1/VuexyTheme/src/@core/layouts/components/AppFooter.vue
@@ -1,20 +1,24 @@
 <template>
-  <p class="clearfix mb-0">
+  <p class="mb-0" style="display: flex; justify-content: space-between;">
     <span class="float-md-left d-block d-md-inline-block mt-25">
       COPYRIGHT  © {{ new Date().getFullYear() }}
       <b-link
-        class="ml-25"
-        href="https://1.envato.market/pixinvent_portfolio"
-        target="_blank"
+          class="ml-25"
+          href="https://1.envato.market/pixinvent_portfolio"
+          target="_blank"
       >Pixinvent</b-link>
       <span class="d-none d-sm-inline-block">, All rights Reserved</span>
     </span>
 
-    <span class="float-md-right d-none d-md-block">Hand-crafted &amp; Made with
+    <span class="float-md-left d-block d-md-inline-block mt-25">
+      I am also here, Hello! <em>-Rene Potocnik</em>
+    </span>
+
+    <span class="">Hand-crafted &amp; Made with
       <feather-icon
-        icon="HeartIcon"
-        size="21"
-        class="text-danger stroke-current"
+          icon="HeartIcon"
+          size="21"
+          class="text-danger stroke-current"
       />
     </span>
   </p>
